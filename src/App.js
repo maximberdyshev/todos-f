e41b8dc6d63e32eedd31ecbf66f10cc15e428f78
Maksim Berdyshev <m.n.berdyshev@it.etagi.com>
2022-07-20 11:20:23 +0500

    Перенес первоначальное состояние todos в другой компонент, в App.js должны остаться только роуты

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './styles/App.css'
 import React, { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { AuthContext } from './context/MyContext'
-import MyNavbar from './components/Navbar/MyNavbar'
 import Login from './pages/Login'
 import Todos from './pages/Todos'
 import Error from './pages/Error'
@@ -24,35 +23,7 @@ function App() {
     userPass: '',
   })
 
-  // имитация БД с задачами
-  let [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: 'Заголовок задачи 1',
-      body: 'Описание задачи 1',
-      dateEnd: '2022-10-10',
-      dateCreate: '2022-01-01',
-      dateUpdate: '2022-05-05',
-      priority: 'средний',
-      completed: false,
-      initiator: 'Smith',
-      executor: 'Smith',
-    },
-    {
-      id: 2,
-      title: 'Заголовок задачи 2',
-      body: 'Описание задачи 2',
-      dateEnd: '2022-07-01',
-      dateCreate: '2022-01-22',
-      dateUpdate: '2022-05-13',
-      priority: 'средний',
-      completed: false,
-      initiator: 'Smith',
-      executor: 'Smith',
-    },
-  ])
-
-  const value = {
+  const authValue = {
     isAuth,
     setIsAuth,
     userData,
@@ -62,15 +33,11 @@ function App() {
 
   return (
     <div>
-      <AuthContext.Provider value={value}>
+      <AuthContext.Provider value={authValue}>
         {localStorage.getItem('authorized', '1') ? (
           <div className='App'>
-            <MyNavbar todos={todos} setTodos={setTodos} />
             <Routes>
-              <Route
-                path='/todo'
-                element={<Todos todos={todos} setTodos={setTodos} />}
-              />
+              <Route path='/todo' element={<Todos />} />
               <Route path='/error' element={<Error />} />
               <Route path='*' element={<Navigate to='/todo' />} />
             </Routes>
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoList from '../components/Todo/TodoList'
+import MyNavbar from '../components/Navbar/MyNavbar'
 
-const Todos = ({ todos, setTodos }) => {
+const Todos = () => {
+  // имитация БД с задачами
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Заголовок задачи 1',
+      body: 'Описание задачи 1',
+      dateEnd: '2022-10-10',
+      dateCreate: '2022-01-01',
+      dateUpdate: '2022-05-05',
+      priority: 'средний',
+      completed: false,
+      initiator: 'Smith',
+      executor: 'Smith',
+    },
+    {
+      id: 2,
+      title: 'Заголовок задачи 2',
+      body: 'Описание задачи 2',
+      dateEnd: '2022-07-01',
+      dateCreate: '2022-01-22',
+      dateUpdate: '2022-05-13',
+      priority: 'средний',
+      completed: false,
+      initiator: 'Smith',
+      executor: 'Smith',
+    },
+  ])
+
   return (
-    <div className='Todos'>
-      {todos.length === 0 ? (
-        <h1 style={{ textAlign: 'center', marginTop: '150px' }}>
-          Задачи не найдены
-        </h1>
-      ) : (
-        <TodoList todos={todos} setTodos={setTodos} />
-      )}
+    <div style={{ alignContent: 'center' }}>
+      <MyNavbar todos={todos} setTodos={setTodos} />
+      <div className='Todos'>
+        {todos.length === 0 ? (
+          <h1 style={{ textAlign: 'center', marginTop: '150px' }}>
+            Задачи не найдены
+          </h1>
+        ) : (
+          <TodoList todos={todos} setTodos={setTodos} />
+        )}
+      </div>
     </div>
   )
 }
